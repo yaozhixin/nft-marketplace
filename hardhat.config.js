5c9dd8f43fd52409939d303123bfe180fab85d11
yaozhixin <zhixiny@graphcore.ai>
2023-02-01 16:48:10 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
+
+// Goerli testnet
+const ALCHEMY_API_KEY = "rYGCIHTsrmwTW5B-ECCvio6mX84v9cAn";
+const GOERLI_PRIVATE_KEY = "d1980fc7870601b15cda3c15ec50d690ef59447251e7c5debec72747b7241ec9";
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  },
   paths: {
     artifacts: "./src/backend/artifacts",
     sources: "./src/backend/contracts",
